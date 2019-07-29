aa39d8c58ab9efdc113fbb7c873fc30180bf525d
Nevimax <nevimax@gmail.com>
2019-07-29 17:08:29 +0300

    end

--- app/src/main/java/com/nevimax/nmynotes/NoteViewHolder.java
+++ app/src/main/java/com/nevimax/nmynotes/NoteViewHolder.java
@@ -13,6 +13,7 @@ public class NoteViewHolder extends RecyclerView.ViewHolder {
 
     public NoteViewHolder(@NonNull View itemView) {
         super(itemView);
+//        itemView.setOnLongClickListener(MainActivity.c);
         tvName = itemView.findViewById(R.id.tv_name);
         tvTime = itemView.findViewById(R.id.tv_time);
         tvText = itemView.findViewById(R.id.tv_text);
